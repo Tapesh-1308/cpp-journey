00e3f66b210a25d74032b593589e4d8130e4a7f8
Tapesh <tapesh.dua420@gmail.com>
2022-01-03 18:22:50 +0530

    update

---
+++ greedyAlgoProblems/minMaxDiff.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n; cin>>n;
+    vector<int> a(n);
+    for(auto &it: a) cin>>it;
+    sort(a.begin(), a.end());
+    long long mn=0, mx=0;
+    for(int i=0; i<n/2; i++){
+        mx += (a[i+n/2] - a[i]);
+        mn += (a[2*i+1] - a[2*i]);
+    }
+    cout<<mn<<" "<<mx<<endl;
+    return 0;
+}
